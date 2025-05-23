a83ae3293b6be7cf0a4bbfe543e8b534959f850a
mohamed <mohamed.nasef19@gmail.com>
2025-05-23 20:53:47 +0300

    mimac-usesd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,14 @@ const Home = () => {
     <div className="min-w-[95vw] overflow-hidden min-h-[100vh] relative  flex justify-center  items-center flex-col">
   <Helmet>
         <title>ماكينات بيتي فور ميماك ايطالي مستملة</title>
-        <meta name="description" content="ماكينات بيتي فور ايطالي ميماك مستعملة
-       petit four italy machine mimac used" />
+        <meta name="description" content="ماكينات بيتي فور ايطالي ميماك مستعملة, ماكينة بيتي فور مستعملة, ماكينة ايطالي بيتي فور
+       petit four italy machine mimac used,
+       Used Mimac Petit Four machine for sale,
+       Second-hand Mimac biscuit machine,
+       Buy Mimac pastry equipment in Egypt,
+       Italian Mimac bakery machine used,
+       Mimac Petit Four depositor second-hand
+       " />
     </Helmet>
          <Intro/>
          <ProductsShow/>
